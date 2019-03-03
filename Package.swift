a916e6fe8b4dbe011ce3c3c2f1300ec6105e2b05
Wilson Gramer <12683684+Wilsonator5000@users.noreply.github.com>
2019-03-03 08:37:44 -0500

    Temporarily remove platform requirements

--- Package.swift
+++ Package.swift
@@ -3,12 +3,6 @@
 import PackageDescription
 
 let pkg = Package(name: "PromiseKit")
-pkg.platforms = [
-    .macOS(.v10_12), //FIXME strictly 10.10 (only tests need 10.12)
-    .iOS(.v10),      //FIXME strictly 8.0
-    .tvOS(.v10),     //FIXME strictly 9.0
-    .watchOS(.v2)
-]
 pkg.products = [
     .library(name: "PromiseKit", targets: ["PromiseKit"]),
 ]
